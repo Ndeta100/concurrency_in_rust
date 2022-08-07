31ba879eec2d42ecdee9e1d746a92132f49629d4
Ndeta Innocent <you@example.com>
2022-08-07 15:20:15 +0300

    Shared state concurrnecy with Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{sync::mpsc, thread, time::Duration};
+use std::{
+    sync::{mpsc, Mutex},
+    thread,
+    time::Duration,
+};
 
 fn main() {
     //Creating a new thread with spawn
@@ -22,13 +26,41 @@ fn main() {
     handle_me.join().unwrap();
     //Using channels to pass messages
     let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
     thread::spawn(move || {
-        let val = String::from("Hi Ndeta");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("Hi"),
+            String::from("there"),
+            String::from("from "),
+            String::from("Github"),
+        ];
+        for i in vals {
+            tx1.send(i).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
-    let received = match rx.recv() {
-        Ok(msg) => msg,
-        Err(e) => panic!("Nothing was received, got error {}", e),
-    };
-    println!("Got {}", received);
+
+    //Second thread
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("More"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you!"),
+        ];
+        for i in vals {
+            tx.send(i).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    for received in rx {
+        println!(" {}", received);
+    }
+    //Shared state concurrency
+    let m = Mutex::new(5);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    println!("m= {:?}", m);
 }
